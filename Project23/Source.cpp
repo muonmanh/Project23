00af8dfb4bea38dc871a1ba02a756fdcfee97735
muonramanh <muonramanh@gmail.com>
2019-02-01 08:04:11 +0700

    add input

--- Project23/Source.cpp
+++ Project23/Source.cpp
@@ -3,7 +3,16 @@ using namespace std;
 struct fraction {
 	int x;
 	int y;
+	void input();
 };
+void fraction::input() {
+	cout << "Enter x: ";
+	cin >> x;
+
+	cout << "Enter y: ";
+	cin >> y;
+
+}
 void main()
 {
 	

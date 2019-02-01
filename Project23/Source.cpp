52cc5c041d1896154477b8dd99045f084e5095f1
muonramanh <muonramanh@gmail.com>
2019-02-01 08:11:14 +0700

    changed x to num

--- Project23/Source.cpp
+++ Project23/Source.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 struct fraction {
-	int x;
+	int num;
 	int y;
 	void input();
+	void output();
 };
 void fraction::input() {
-	cout << "Enter x: ";
-	cin >> x;
+	cout << "Enter num: ";
+	cin >> num;
 
 	cout << "Enter y: ";
 	cin >> y;
 
+}
+void fraction::output()
+{
+	cout << num;
+	cout << y;
 }
 void main()
 {
